5c435f7b457cab182956f9c8de8fb545d3e2a921
unknown <mrollberg@ymail.com>
2017-09-17 20:39:12 -0500

    Formatted code and closed connections

--- Data-Access-Objects/DataAccessObjects/src/main/java/com/cooksys/dao/InterestDao.java
+++ Data-Access-Objects/DataAccessObjects/src/main/java/com/cooksys/dao/InterestDao.java
@@ -11,89 +11,79 @@ import java.util.NoSuchElementException;
 import com.cooksys.entity.Interest;
 
 public class InterestDao {
-	
-	private final String JDBC_DRIVER = "org.postgresql.Driver";  
-    private final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
-    private final String USER = "postgres";
-    private final String PASS = "bondstone";
-	
-	public Interest get(Long id)
-	{
+
+	private final String JDBC_DRIVER = "org.postgresql.Driver";
+	private final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+	private final String USER = "postgres";
+	private final String PASS = "bondstone";
+
+	public Interest get(Long id) {
 		Interest interest = new Interest();
 		try {
 			ResultSet rs = query("SELECT * FROM \"Interest\" WHERE id = " + id);
-			
+
 			while (rs.next()) {
-			    interest.setId(id);
-			    interest.setTitle(rs.getString("title"));;
+				interest.setId(id);
+				interest.setTitle(rs.getString("title"));
 			}
-        
-        } catch (SQLException e) {
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return interest;
 	}
-	
-	public void save(Interest interest)
-	{
-		if (interest == null)
-		{
+
+	public void save(Interest interest) {
+		if (interest == null) {
 			return;
 		}
-		
-		if (interest.getId() == null)
-		{
+
+		if (interest.getId() == null) {
 			try {
-				 Connection connection = getConnection();
-				 String SQL_INSERT = "insert into \"Interest\" (title) values (?)";
-			     PreparedStatement statement = connection.prepareStatement(SQL_INSERT,
-					                                  Statement.RETURN_GENERATED_KEYS);
+				Connection connection = getConnection();
+				String SQL_INSERT = "insert into \"Interest\" (title) values (?)";
+				PreparedStatement statement = connection.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);
+
+				statement.setString(1, interest.getTitle());
+
+				int affectedRows = statement.executeUpdate();
 				
-			     statement.setString(1, interest.getTitle());
+				connection.close();
 
-				 int affectedRows = statement.executeUpdate();
+				if (affectedRows == 0) {
+					throw new SQLException("Creating user failed, no rows affected.");
+				}
 
-				 if (affectedRows == 0) {
-					 throw new SQLException("Creating user failed, no rows affected.");
-				 }
-
-				 try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
-					 if (generatedKeys.next()) {
-						 interest.setId(generatedKeys.getLong(1));
-					 }
-					 else {
-						 throw new SQLException("Creating user failed, no ID obtained.");
-					 }
-				 }
-				 connection.close();
+				try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+					if (generatedKeys.next()) {
+						interest.setId(generatedKeys.getLong(1));
+					} else {
+						throw new SQLException("Creating user failed, no ID obtained.");
+					}
+				}
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		else
-		{
+		} else {
 			Long iid = interest.getId();
 			String title = interest.getTitle();
-			
+
 			int numUpdates = update("UPDATE \"Interest\" SET title = '" + title + "' WHERE id = " + iid);
-			
-			if (numUpdates == 0)
-			{
+
+			if (numUpdates == 0) {
 				throw new NoSuchElementException();
 			}
 		}
 	}
-	
-	private Connection getConnection()
-	{
+
+	private Connection getConnection() {
 		Connection conn = null;
-		
-		//STEP 2: Register JDBC driver
+
+		// STEP 2: Register JDBC driver
 		try {
 			Class.forName(JDBC_DRIVER);
-			//STEP 3: Open a connection
+			// STEP 3: Open a connection
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -102,13 +92,12 @@ public class InterestDao {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return conn;
 	}
-	
-	private ResultSet query(String query)
-	{
-		
+
+	private ResultSet query(String query) {
+
 		try {
 			Connection conn = getConnection();
 			Statement stmt = conn.createStatement();
@@ -119,12 +108,11 @@ public class InterestDao {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
-	private Integer update(String query)
-	{
+
+	private Integer update(String query) {
 		try {
 			Connection conn = getConnection();
 			Statement stmt = conn.createStatement();
@@ -135,7 +123,7 @@ public class InterestDao {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 
--- Data-Access-Objects/DataAccessObjects/src/main/java/com/cooksys/dao/PersonDao.java
+++ Data-Access-Objects/DataAccessObjects/src/main/java/com/cooksys/dao/PersonDao.java
@@ -15,70 +15,57 @@ import com.cooksys.entity.Location;
 import com.cooksys.entity.Person;
 
 public class PersonDao {
-	
-	private final String JDBC_DRIVER = "org.postgresql.Driver";  
-    private final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
-    private final String USER = "postgres";
-    private final String PASS = "bondstone";
-    
-	
-	
-	public Person get(Long id)
-	{
+
+	private final String JDBC_DRIVER = "org.postgresql.Driver";
+	private final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+	private final String USER = "postgres";
+	private final String PASS = "bondstone";
+
+	public Person get(Long id) {
 		Person person = new Person();
 		try {
-			ResultSet rs = query("SELECT DISTINCT ON(pid) " +
-					"\"Person\".id as pid, \"firstName\", \"lastName\", age, " +
-					"\"Location\".id as lid, city, state, country " + 
-					"from \"Person\" " +
-					"INNER JOIN \"Location\" " +
-					"ON \"Person\".location_id = \"Location\".id " +
-					"WHERE \"Person\".id = " + id);
-			
+			ResultSet rs = query("SELECT DISTINCT ON(pid) " + "\"Person\".id as pid, \"firstName\", \"lastName\", age, "
+					+ "\"Location\".id as lid, city, state, country " + "from \"Person\" " + "INNER JOIN \"Location\" "
+					+ "ON \"Person\".location_id = \"Location\".id " + "WHERE \"Person\".id = " + id);
+
 			while (rs.next()) {
-			    person.setId(id);
-			    person.setFirstName(rs.getString("firstName"));
-			    person.setLastName(rs.getString("lastName"));
-			    person.setAge(rs.getInt("age"));
-			    Location location = new Location();
-			    location.setId(rs.getLong("lid"));
-			    location.setCity(rs.getString("city"));
-			    location.setState(rs.getString("state"));
-			    location.setCountry(rs.getString("country"));
-			    person.setLocation(location);
+				person.setId(id);
+				person.setFirstName(rs.getString("firstName"));
+				person.setLastName(rs.getString("lastName"));
+				person.setAge(rs.getInt("age"));
+				Location location = new Location();
+				location.setId(rs.getLong("lid"));
+				location.setCity(rs.getString("city"));
+				location.setState(rs.getString("state"));
+				location.setCountry(rs.getString("country"));
+				person.setLocation(location);
 			}
-			
-	        ResultSet interestsSet = query("SELECT \"Interest\".id as iid, title " +
-	    	        "from \"Person\" " +
-	    	        "INNER JOIN \"PersonInterest\" " + 
-	    	        "ON \"Person\".id = \"PersonInterest\".person_id " +
-	    	        "INNER JOIN \"Interest\" " + 
-	    	        "ON \"PersonInterest\".interest_id = \"Interest\".id " +
-	    	        "WHERE \"Person\".id = " + id);
-	        Set<Interest> interests = new HashSet<Interest>();
-	        while (interestsSet.next())
-	        {
-	        	Interest interest = new Interest();
-	        	interest.setId(interestsSet.getLong("iid"));
-	        	interest.setTitle(interestsSet.getString("title"));
-	        	interests.add(interest);
-	        }
-	        person.setInterests(interests);
-        
-        } catch (SQLException e) {
+
+			ResultSet interestsSet = query(
+					"SELECT \"Interest\".id as iid, title " + "from \"Person\" " + "INNER JOIN \"PersonInterest\" "
+							+ "ON \"Person\".id = \"PersonInterest\".person_id " + "INNER JOIN \"Interest\" "
+							+ "ON \"PersonInterest\".interest_id = \"Interest\".id " + "WHERE \"Person\".id = " + id);
+			Set<Interest> interests = new HashSet<Interest>();
+			while (interestsSet.next()) {
+				Interest interest = new Interest();
+				interest.setId(interestsSet.getLong("iid"));
+				interest.setTitle(interestsSet.getString("title"));
+				interests.add(interest);
+			}
+			person.setInterests(interests);
+
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return person;
 	}
-	
-	public void save(Person person)
-	{
-		if (person == null)
-		{
+
+	public void save(Person person) {
+		if (person == null) {
 			return;
 		}
-		
+
 		LocationDao locationDao = new LocationDao();
 		locationDao.save(person.getLocation());
 
@@ -87,21 +74,22 @@ public class PersonDao {
 		for (Interest interest : interests) {
 			interestDao.save(interest);
 		}
-		
-		if (person.getId() == null) 
-		{
+
+		if (person.getId() == null) {
 			try {
 				Connection connection = getConnection();
-				String SQL_INSERT = "insert into \"Person\" (\"firstName\", \"lastName\", age, location_id) values (?, ?, ?, ?)";
+				String SQL_INSERT = "insert into \"Person\" (\"firstName\", \"lastName\", age, location_id) "
+						+ "values (?, ?, ?, ?)";
 				PreparedStatement statement = connection.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);
 
 				statement.setString(1, person.getFirstName());
 				statement.setString(2, person.getLastName());
 				statement.setLong(3, person.getAge());
 				statement.setLong(4, person.getLocation().getId());
-				// ...
 
 				int affectedRows = statement.executeUpdate();
+
+				connection.close();
 
 				if (affectedRows == 0) {
 					throw new SQLException("Creating user failed, no rows affected.");
@@ -114,117 +102,88 @@ public class PersonDao {
 						throw new SQLException("Creating user failed, no ID obtained.");
 					}
 				}
-				
-				for (Interest interest : interests)
-				{
+
+				for (Interest interest : interests) {
 					savePersonInterests(person.getId(), interest.getId());
 				}
-				
 
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		} 
-		else 
-		{
-
-			
-			
+		} else {
 			Long pid = person.getId();
 			String firstName = person.getFirstName();
 			String lastName = person.getLastName();
 			Integer age = person.getAge();
 			Long locationId = person.getLocation().getId();
 
-			int numUpdates = update("UPDATE \"Person\" " + 
-					"SET \"firstName\" = '" + firstName + "', \"lastName\" = '" + lastName + "', " + 
-					"age = " + age + ", location_id = " + locationId + " " + 
-					"WHERE id = " + pid);
+			int numUpdates = update("UPDATE \"Person\" " + "SET \"firstName\" = '" + firstName + "', \"lastName\" = '"
+					+ lastName + "', " + "age = " + age + ", location_id = " + locationId + " " + "WHERE id = " + pid);
 
 			if (numUpdates == 0) {
 				throw new NoSuchElementException();
 			}
-			
-			update("DELETE FROM \"PersonInterest\" " +
-					"WHERE person_id = " + person.getId());
+
+			update("DELETE FROM \"PersonInterest\" " + "WHERE person_id = " + person.getId());
+
 			for (Interest interest : interests) {
 				savePersonInterests(person.getId(), interest.getId());
 			}
 		}
 	}
-	
-	public Set<Person> findInterestGroup(Interest interest, Location location)
-	{
-		
+
+	public Set<Person> findInterestGroup(Interest interest, Location location) {
 		Set<Person> people = new HashSet<Person>();
 		try {
 			Long locationId = location.getId();
 			Long interestId = interest.getId();
-			
-			ResultSet rs = query("SELECT " +
-					"\"Person\".id as pid, \"firstName\", \"lastName\", age, " +
-					"\"Location\".id as lid, city, state, country " +  
-					"from \"Person\" " +
-					"INNER JOIN \"PersonInterest\" " +
-					"ON \"Person\".id = \"PersonInterest\".person_id " +
-					"INNER JOIN \"Interest\" " +
-					"ON \"PersonInterest\".interest_id = \"Interest\".id " +
-					"INNER JOIN \"Location\" " +
-					"ON \"Person\".location_id = \"Location\".id " +
-					"where \"Interest\".id = " + interestId + " AND \"Location\".id = " + locationId);
-			
-			
-			while (rs.next())
-			{
+
+			ResultSet rs = query("SELECT " + "\"Person\".id as pid, \"firstName\", \"lastName\", age, "
+					+ "\"Location\".id as lid, city, state, country " + "from \"Person\" "
+					+ "INNER JOIN \"PersonInterest\" " + "ON \"Person\".id = \"PersonInterest\".person_id "
+					+ "INNER JOIN \"Interest\" " + "ON \"PersonInterest\".interest_id = \"Interest\".id "
+					+ "INNER JOIN \"Location\" " + "ON \"Person\".location_id = \"Location\".id "
+					+ "where \"Interest\".id = " + interestId + " AND \"Location\".id = " + locationId);
+
+			while (rs.next()) {
 				Person person = new Person();
 				person.setId(rs.getLong("pid"));
-			    person.setFirstName(rs.getString("firstName"));
-			    person.setLastName(rs.getString("lastName"));
-			    person.setAge(rs.getInt("age"));
-			    Location loc = new Location();
-			    loc.setId(rs.getLong("lid"));
-			    loc.setCity(rs.getString("city"));
-			    loc.setState(rs.getString("state"));
-			    loc.setCountry(rs.getString("country"));
-			    person.setLocation(loc);
-			    
-			    ResultSet interestsSet = query("SELECT \"Interest\".id as iid, title " +
-		    	        "from \"Person\" " +
-		    	        "INNER JOIN \"PersonInterest\" " + 
-		    	        "ON \"Person\".id = \"PersonInterest\".person_id " +
-		    	        "INNER JOIN \"Interest\" " + 
-		    	        "ON \"PersonInterest\".interest_id = \"Interest\".id " +
-		    	        "WHERE \"Person\".id = " + rs.getLong("pid"));
-			    Set<Interest> interests = new HashSet<Interest>();
-		        while (interestsSet.next())
-		        {
-		        	Interest ist = new Interest();
-		        	ist.setId(interestsSet.getLong("iid"));
-		        	ist.setTitle(interestsSet.getString("title"));
-		        	interests.add(ist);
-		        }
-		        person.setInterests(interests);
-			    
+				person.setFirstName(rs.getString("firstName"));
+				person.setLastName(rs.getString("lastName"));
+				person.setAge(rs.getInt("age"));
+				Location loc = new Location();
+				loc.setId(rs.getLong("lid"));
+				loc.setCity(rs.getString("city"));
+				loc.setState(rs.getString("state"));
+				loc.setCountry(rs.getString("country"));
+				person.setLocation(loc);
+
+				ResultSet interestsSet = query("SELECT \"Interest\".id as iid, title " + "from \"Person\" "
+						+ "INNER JOIN \"PersonInterest\" " + "ON \"Person\".id = \"PersonInterest\".person_id "
+						+ "INNER JOIN \"Interest\" " + "ON \"PersonInterest\".interest_id = \"Interest\".id "
+						+ "WHERE \"Person\".id = " + rs.getLong("pid"));
+				Set<Interest> interests = new HashSet<Interest>();
+				while (interestsSet.next()) {
+					Interest ist = new Interest();
+					ist.setId(interestsSet.getLong("iid"));
+					ist.setTitle(interestsSet.getString("title"));
+					interests.add(ist);
+				}
+				person.setInterests(interests);
+
 				people.add(person);
 			}
-			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
 		return people;
 	}
-	
-	private void savePersonInterests(Long pid, Long iid)
-	{
-		
+
+	private void savePersonInterests(Long pid, Long iid) {
 		try {
-			
-			
 			Connection connection = getConnection();
 			String SQL_INSERT = "insert into \"PersonInterest\" (person_id, interest_id) values (?, ?)";
 			PreparedStatement statement = connection.prepareStatement(SQL_INSERT);
@@ -232,20 +191,19 @@ public class PersonDao {
 			statement.setLong(1, pid);
 			statement.setLong(2, iid);
 			statement.executeUpdate();
+			connection.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	private Connection getConnection()
-	{
+
+	private Connection getConnection() {
 		Connection conn = null;
-		
-		//STEP 2: Register JDBC driver
+		// STEP 2: Register JDBC driver
 		try {
 			Class.forName(JDBC_DRIVER);
-			//STEP 3: Open a connection
+			// STEP 3: Open a connection
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -254,13 +212,11 @@ public class PersonDao {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return conn;
 	}
-	
-	private ResultSet query(String query)
-	{
-		
+
+	private ResultSet query(String query) {
 		try {
 			Connection conn = getConnection();
 			Statement stmt = conn.createStatement();
@@ -271,12 +227,11 @@ public class PersonDao {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
-	private Integer update(String query)
-	{
+
+	private Integer update(String query) {
 		try {
 			Connection conn = getConnection();
 			Statement stmt = conn.createStatement();
@@ -287,8 +242,8 @@ public class PersonDao {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 }
